e7390f180ad3b91d25ae25d7e7262cb4133897cd
Nidansingh202 <107486940+Nidansingh202@users.noreply.github.com>
2024-01-23 02:13:37 +0530

    code

---
+++ project-1/src/App.jsx
@@ -0,0 +1,37 @@
+import"./App.css";
+const App = () => {
+    return(
+        
+
+<div>
+        <ul>
+
+            <li href ="#">About</li>
+            <li href ="#">lens</li>
+            <li href ="#">lensAF</li>
+            <li href ="#">Capture time</li>
+            <li href ="#">ISO</li>
+            <li href ="#">Speed</li>
+            <li href ="#">Apertur</li>
+            <li href ="#">fileName</li>
+            <li href ="#">Image</li>
+            <li href ="#">while Balance</li>
+            <li href ="#">Rating</li>
+            <li href ="#">colur</li>
+            <li href ="#">camera</li>
+        
+
+       <button>Dounload</button> 
+    </ul>
+
+    <div className="pix-image">
+            <img src="/image/Filter.png" alt="Filter-image" />
+            <img src="/image/sky road.png" alt="sky-image" />
+            <img src="/image/sky.png" alt="sky-image" />
+</div>
+</div>
+    
+    );
+};
+
+export default App;
---
+++ project-1/src/component/pix.jsx
@@ -0,0 +1,20 @@
+const pixSection=() =>{
+    return(
+<main className="pix">
+    <div className="pix-content">
+        <h1>
+            your best image in pixcel
+        </h1>
+        <div className="pix-btn">
+            <button>Dounload</button>
+        </div>
+        <div className="pix-image">
+            <img src="/image/Filter.png" alt="Filter-image" />
+            <img src="/image/sky road.png" alt="sky-image" />
+            <img src="/image/sky.png" alt="sky-image" />
+        </div>
+        </div>
+        </main>
+);
+}
+export default pixSection;
